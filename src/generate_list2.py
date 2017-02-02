d81a044863adf3028388c2c70d7201ac20ad58f8
Kittima Mekhabunchakij <kittima.me@gmail.com>
2017-02-02 16:58:07 +0700

    First commit - cpe-341/Test1

---
+++ src/generate_list2.py
@@ -0,0 +1,25 @@
+﻿import random
+def generate_list():
+    alist = [x for x in range(random.randint(-10, 10)) ]
+    assert len(alist) > 0, "A generated list is empty!"
+    assert sum(alist) >= 0, "the Sum of a generated list of number is less than zero!!" 
+    return alist
+    
+    
+""" 
+print a generated list
+"""
+def printIt():
+    mylist = generate_list()
+    print( mylist )    
+    
+def main():
+    """business logic for when running this module as the primary one!"""
+    printIt()
+
+"""
+If this script file is called, run main() directly
+""" 
+if __name__ == '__main__':
+    print( "Test printIt():" )
+    main()
---
+++ src/genlist_test2.py
@@ -0,0 +1,13 @@
+﻿#! Insert the current directory path to Python Path
+import sys
+import os
+cwd = os.getcwd()
+
+sys.path.append(cwd)
+#print(sys.path)
+
+# Test the module: generate_list
+from generate_list2 import printIt
+
+for _ in range(100):
+    printIt()
